c659932eea3185b71276c591170e9f8222bc170b
Vladimus22 <68963869+Vladimus22@users.noreply.github.com>
2020-07-31 03:27:24 +0300

    c

--- config.js
+++
@@ -1,4 +0,0 @@
-var config = {
-    API_KEY:'149234349fa5230cb31be186bd245a3d'
-}
-
--- script.js
+++
@@ -1,8 +0,0 @@
-function change_bg_color(name_id, hex_color, text_color){
-
-    var el = document.getElementById(name_id);
-    el.style.transition = 'background 1.0s linear 0s';
-    el.style.backgroundColor = hex_color;
-    el.style.color = text_color;    
-    
-}
